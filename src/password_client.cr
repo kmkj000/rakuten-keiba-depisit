3cd00370062e5a833268ca6eb53a941f0e9c930e
kmkj000 <kd1191681@st.kobedenshi.ac.jp>
2022-06-27 00:44:18 +0900

    [update] Salt length to random

--- src/password_client.cr
+++ src/password_client.cr
@@ -4,8 +4,11 @@ module Rakuten::Keiba::Deposit
 
     def initialize(@unidentified_password : String , @salt_path : String)
       if !File.exists?(@salt_path)
-        File.write(@salt_path, Random::Secure.hex)
+        File.write(@salt_path, Random::Secure.hex(Random.rand(16)))
         p "Created salt file: " + @salt_path
+      end
+      if File.read(@salt_path).size == 0
+        File.write(@salt_path, Random::Secure.hex(Random.rand(16)))
       end
 
       p "Use salt file: " + @salt_path
