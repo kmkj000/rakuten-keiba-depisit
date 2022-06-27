d366da9adc2bb9c3c39187eab84b71ab237dd02d
kmkj000 <kd1191681@st.kobedenshi.ac.jp>
2022-06-27 09:06:05 +0900

    [update] Change encrypt rule

--- src/password_client.cr
+++ src/password_client.cr
@@ -4,13 +4,13 @@ module Rakuten::Keiba::Deposit
   class PasswordClient
     @salt = ""
 
-    def initialize(@unidentified_password : String , @salt_path : String)
+    def initialize(@input : String , @salt_path : String)
       if !File.exists?(@salt_path)
-        File.write(@salt_path, Random::Secure.urlsafe_base64(Random.rand(16)))
+        File.write(@salt_path, create_salt)
         p "Created salt file: " + @salt_path
       end
       if File.read(@salt_path).size == 0
-        File.write(@salt_path, Random::Secure.urlsafe_base64(Random.rand(16)))
+        File.write(@salt_path, create_salt)
       end
 
       p "Use salt file: " + @salt_path
@@ -21,13 +21,21 @@ module Rakuten::Keiba::Deposit
     end
 
     def encrypt()
-      base64_password = Base64.urlsafe_encode(@unidentified_password,padding = false)
-
+      base64_password = Base64.urlsafe_encode(@input, padding = false)
       front_size = Random.rand(base64_password.size - 1)
       front_password = base64_password[0..front_size]
       rear_password = base64_password[(front_size + 1)..]
 
-      front_password + @salt + rear_password
+      split_size = (@salt.size / 3).to_i
+      front_salt = @salt[0..(split_size - 1)]
+      middle_salt = @salt[split_size..(split_size * 2)]
+      rear_salt = @salt[((split_size * 2) + 1)..]
+
+      front_salt + front_password + middle_salt + rear_password + rear_salt
+    end
+
+    private def create_salt
+      Random::Secure.urlsafe_base64(Random.rand(6..32), padding = false)
     end
   end
 end
